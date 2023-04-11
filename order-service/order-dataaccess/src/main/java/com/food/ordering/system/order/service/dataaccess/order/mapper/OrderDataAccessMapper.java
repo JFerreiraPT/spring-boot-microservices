a6162132bfa29723fbd7efd5e5b874fe3516b790
João Ferreira <joao.p.r.fereira.ofgmail.com>
2023-04-11 15:38:53 +0100

    feat: add code for order repository

--- order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/order/mapper/OrderDataAccessMapper.java
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/order/mapper/OrderDataAccessMapper.java
@@ -20,7 +20,7 @@ import java.util.stream.Collectors;
 import static com.food.ordering.system.order.service.domain.entity.Order.FAILURE_MESSAGE_DELIMITER;
 
 @Component
-public class orderDataAccessMapper {
+public class OrderDataAccessMapper {
 
     public OrderEntity orderToOrderEntity(Order order) {
         OrderEntity orderEntity = OrderEntity.builder()
