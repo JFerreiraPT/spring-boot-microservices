190ff7fb8c837995a965721251976cb2b4f85655
João Ferreira <joao.p.r.fereira.ofgmail.com>
2023-04-11 15:32:51 +0100

    Add infra and controllers and data access implementation

---
+++ infrastructure/kafka/kafka-consumer/src/main/java/com/food/ordering/system/kafka/consumer/KafkaConsumer.java
@@ -0,0 +1,9 @@
+package com.food.ordering.system.kafka.consumer;
+
+import org.apache.avro.specific.SpecificRecord;
+
+import java.util.List;
+
+public interface KafkaConsumer<T extends SpecificRecord> {
+    void receive(List<T> message, List<Long> keys, List<Integer> partitions, List<Long> offsets);
+}
